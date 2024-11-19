80f9baad832869585bd38d857b9fd797504ecd6a
parkwoobin <oi3oi3oi@naver.com>
2024-11-20 00:56:05 +0900

    startup.sh 파일 수정: Streamlit 설치 추가

--- startup.sh
+++ startup.sh
@@ -3,4 +3,5 @@
 source /opt/conda/bin/activate /home/site/conda/envs/yoloCuda_py311  # Anaconda 환경 활성화
 
 # Streamlit 실행 (80번 포트에서 실행)
+pip install streamlit
 streamlit run app.py --server.address=0.0.0.0 --server.port=80
